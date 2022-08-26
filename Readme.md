6e8f16dc8a9371d57b8ad077f68b801bb6ea9f77
abevol <abevol@pm.me>
2022-08-26 16:49:08 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# 标签自动分组
+
+## 效果图
+
+![效果图片](./readme/main.jpg)
+
+## 使用
+
+1. 通过 Microsoft Edge 扩展商店安装：[标签页自动分组](https://microsoftedge.microsoft.com/addons/detail/%E6%A0%87%E7%AD%BE%E9%A1%B5%E8%87%AA%E5%8A%A8%E5%88%86%E7%BB%84/hifeijficcakiokpfabdflnjdbihofdd)
+2. 通过 Github 下载此项目源码 或者在 Release 中下载压缩包。
+   打开扩展页面的 开发人员模式。
+   选择加载解压缩的扩展。
+
+## 功能
+
+1. 安装后会对已有的标签页自动进行分组
+2. 每当打开新标签页会自动对其进行分组
+
+## 更新日志
+
+### 2022.08.26
+
+初始版本
+
+## 源码
+
+ <https://github.com/abevol/AutoGroupTabs>
---
+++ background.js
@@ -0,0 +1,127 @@
+chrome.runtime.onInstalled.addListener(function(details)
+{
+    details && "install" == details.reason && groupAllTabs();
+});
+
+chrome.tabs.onCreated.addListener(function(tab)
+{
+    groupTab(tab);
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab)
+{
+    if (changeInfo.url != undefined)
+    {
+        groupTab(tab);
+    }
+});
+
+const colors = ["grey", "blue", "yellow", "red", "green", "pink", "purple", "cyan"]
+
+function genGroupName(url)
+{
+    url = new URL(url);
+    if (url.protocol != "http:" && url.protocol != "https:")
+    {
+        return url.protocol.substr(0, url.protocol.length - 1);
+    }
+    let hostName = url.hostname;
+    let groupName = hostName.startsWith("www.") ? hostName.substr(4) : hostName;
+    return groupName;
+}
+
+let tabIdx = 0;
+let allTabs = [];
+
+function onGroupTabComplete()
+{
+    tabIdx++;
+    if (tabIdx < allTabs.length)
+    {
+        let tab = allTabs[tabIdx];
+        groupTab(tab, onGroupTabComplete);
+    }
+}
+
+function groupAllTabs()
+{
+    console.debug("groupAllTabs");
+    chrome.tabs.query(
+    {
+        currentWindow: true
+    }, function(tabs)
+    {
+        tabIdx = 0;
+        allTabs = tabs;
+        groupTab(allTabs[tabIdx], onGroupTabComplete);
+    });
+}
+
+function groupTab(tab, complete)
+{
+    if (tab.url == "" || tab.pinned)
+    {
+        complete && complete();
+        return;
+    }
+
+    chrome.windows.getCurrent(function(currentWindow)
+    {
+        chrome.tabGroups.query(
+        {
+            windowId: currentWindow.id
+        }, function(groups)
+        {
+            groupTabIntl(tab, groups, currentWindow, complete);
+        })
+    });
+}
+
+function groupTabIntl(tab, groups, currentWindow, complete)
+{
+    try
+    {
+        let groupName = genGroupName(tab.url);
+        const existedGroup = groups.find(a => a.title == groupName);
+        if (existedGroup == undefined)
+        {
+            chrome.tabs.group(
+            {
+                createProperties:
+                {
+                    windowId: currentWindow.id,
+                },
+                tabIds: tab.id
+            }, function(groupId)
+            {
+                console.debug("add group", groupName);
+                chrome.tabGroups.update(groupId,
+                {
+                    color: colors[parseInt(Math.random() * 10)],
+                    title: groupName,
+                }, function(group)
+                {
+                    console.debug("group added", group.title);
+                    complete && complete();
+                });
+            })
+        }
+        else
+        {
+            console.debug("update group", groupName);
+            chrome.tabs.group(
+            {
+                groupId: existedGroup.id,
+                tabIds: tab.id
+            }, function(groupId)
+            {
+                console.debug("group updated", groupName);
+                complete && complete();
+            })
+        }
+    }
+    catch (e)
+    {
+        console.error(e)
+    }
+}
